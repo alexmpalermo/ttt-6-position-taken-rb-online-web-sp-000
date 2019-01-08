901a0bcfe52e1da093451b043761291be50822cd
Alexandra Palermo <alexmpalermo@gmail.com>
2019-01-08 21:12:46 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,8 +2,9 @@
 def position_taken?(board, index)
   if board[index] == "X" || "O"
     true
-   else board[index] == " " || ""
+   elsif board[index] == " " || ""
     false
- 
+else board[index] != "X" || "O"
+  false
  end
 end
